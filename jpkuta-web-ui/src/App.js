828254529cc79c505b190f6994db210423641845
jpkuta <jpkuta@gmail.com>
2023-12-15 21:01:37 -0600

    clean up files

--- jpkuta-web-ui/src/App.js
+++ jpkuta-web-ui/src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import Experience from './pages/Experience';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path='/' element={< Home/>}></Route>
+          <Route path="/" element={< Home/>} />
+          <Route path='/projects' element={< Projects/>} />
+          <Route path='/experience' element={< Experience/>} />
         </Routes>
       </Router>
       
