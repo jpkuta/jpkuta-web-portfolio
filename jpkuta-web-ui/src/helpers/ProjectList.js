9ec4ffd71d40a3bdc0d136b46499316f2cc4d53a
jpkuta <jpkuta@gmail.com>
2023-12-29 00:45:32 -0600

    images added to projects list

--- jpkuta-web-ui/src/helpers/ProjectList.js
+++ jpkuta-web-ui/src/helpers/ProjectList.js
@@ -1,11 +1,15 @@
+import Proj1 from '../assets/GolfcheryLogo.png';
+import Proj2 from '../assets/Zom-Bear-logos.jpeg';
 
 export const ProjectList = [
   {
     name: "Golfchery",
+    image: Proj1,
     skills: "Unity Game Engine, C#, Electric Guitar",
   },
   {
-    name: "Tag Game",
+    name: "Zom-Bear Tag",
+    image: Proj2,
     skills: "Unity Game Engine, C#, Electric Guitar, Bass Guitar",
   },
 
