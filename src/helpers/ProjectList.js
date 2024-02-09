7c211324587f51dbd7c74c7b2919dcccf13b3f38
Justin Kuta <jpkuta@gmail.com>
2024-02-08 23:54:54 -0600

    added audio viz project

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -1,5 +1,6 @@
 import Proj1 from '../assets/GolfcheryLogo.png';
 import Proj2 from '../assets/Zom-Bear-logos.jpeg';
+import Proj3 from '../assets/AudioViz-logos.jpeg';
 
 export const ProjectList = [
   {
@@ -11,6 +12,11 @@ export const ProjectList = [
     name: "Zom-Bear Tag",
     image: Proj2,
     skills: "Unity Game Engine, C#, Electric Guitar, Bass Guitar",
+  },
+  {
+    name: "Audio Visualizer",
+    image: Proj3,
+    skills: "Unity Game Engine, C#, fast fourier transform",
   },
 
 ];
